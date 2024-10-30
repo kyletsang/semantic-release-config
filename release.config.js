9eca079c4d44a3cc03b663c72fb45c3981a9a7c1
kyletsang <6854874+kyletsang@users.noreply.github.com>
2024-10-29 22:37:38 -0700

    feat: revamp config and redo

--- release.config.js
+++ release.config.js
@@ -1,10 +1,29 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
 module.exports = {
-  branches: ['main'],
+  branches: [
+    'main',
+    {
+      name: 'alpha',
+      prerelease: true,
+    },
+    {
+      name: 'beta',
+      prerelease: true,
+    },
+  ],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
     '@semantic-release/npm',
-    '@semantic-release/git',
+    [
+      '@semantic-release/github',
+      {
+        successComment: false,
+        failComment: false,
+        releasedLabels: false,
+      },
+    ],
   ],
 };
